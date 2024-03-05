381e7e4367b244f6ff9f91ce30268720ffd1dd70
KatrinaZuus <katrinazuus@gmail.com>
2024-03-06 00:47:38 +0400

    themes is done

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let themeChangeButton = document.getElementsByClassName("theme-change-button")[0]
 let bodyThemecolor = document.getElementsByTagName("body")[0]
 
+ 
 
 
 themeChangeButton.addEventListener(("input"), (event)=>{
     
     if (event.target.value == 2){
-        bodyThemecolor.classList.add(`lightBackground`)
-        bodyThemecolor.classList.remove(`darkBackground`)
+        bodyThemecolor.classList.add("light")
+        bodyThemecolor.classList.remove("dark")
     } else {
         if (event.target.value == 3){
-        bodyThemecolor.classList.add(`darkBackground`)
+        bodyThemecolor.classList.add("dark")
+        bodyThemecolor.classList.remove("light")
     } else {
         if (event.target.value == 1){
-        bodyThemecolor.classList.remove(`darkBackground`, `lightBackground`)
+        bodyThemecolor.classList.remove("light", "dark")
     }
 }
 }
